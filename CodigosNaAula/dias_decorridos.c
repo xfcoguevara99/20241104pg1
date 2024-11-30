efc825a01f8f52ce67cad10b79bae1f6f92db058
francisco <francisco.g1999@aluno.ifsc.edu.br>
2024-11-30 17:35:00 -0300

    falta terminar dias decorridos

--- CodigosNaAula/dias_decorridos.c
+++ CodigosNaAula/dias_decorridos.c
@@ -11,27 +11,35 @@ int num_dias(int mes, int ano){
         if (mes == 2 && bissexto(ano)){
             resultado = 29;
         } else if (mes % 2){
-            resultado = 30;
-        }else{
             resultado = 31;
+        }else{
+            resultado = 30;
         }
     }else{
         if (mes % 2){
-            resultado = 31;
-        }else{
             resultado = 30;
+        }else{
+            resultado = 31;
         }
     }
 
     return resultado;
 }
+bool eh_data_valida(int dia, int mes, int ano){
+    bool resultado = true;
+    int numero_dias_do_mes;
+    numero_dias_do_mes = num_dias(mes,ano);
+    if ((dia > numero_dias_do_mes) || (mes>12) || (ano>2024)){
+        resultado = false;
+    }
+    return resultado;
+}
 
 int main()
 {
-    int a,b,c;
-    a = bissexto(2000);
-    printf("%d",a);
-    
+    int dia,mes,ano;
+    printf("Entre com a data (dd/mm/aaaa): ");
+    scanf("%d/%d/%d",&dia,&mes,&ano);
 
     return 0;
 }
