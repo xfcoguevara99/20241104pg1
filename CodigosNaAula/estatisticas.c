69b83e7d6972b1844f8674b1eed4ec9c0a58a08a
francisco <francisco.g1999@aluno.ifsc.edu.br>
2025-02-22 09:31:30 -0300

    terminar estatisticas

--- CodigosNaAula/estatisticas.c
+++ CodigosNaAula/estatisticas.c
@@ -6,13 +6,13 @@ typedef struct{
 
 Estatisticas calculcula_estatisticas(int nro_elementos, float arranjo[]){
     Estatisticas estatisticas = {0,INFINITY,-INFINITY};
-    for (int == 0;i<nro_elementos;i++){
+    for (int = 0;i<nro_elementos;i++){
         estatisticas.media += arranjo[i];
         if (arranjo[i]>estatisticas.maximo){
-            Estatisticas.maximo = arranjo[i];
+            estatisticas.maximo = arranjo[i];
         }
         if (arranjo[i]<estatisticas.minimo){
-            Estatisticas.minimo = arranjo[i];
+            estatisticas.minimo = arranjo[i];
         }
         estatisticas.media = estatisticas.media / nro_elementos;
     }
@@ -25,12 +25,13 @@ int main(){
     float a[100];
     printf("Entre com o número de elementos (máx 100):");
     scanf("%d",&nro_elementos);
-    for (int i==0;i<nro_elementos;i++){
-        scanf
-
-
+    for (int i=0;i<nro_elementos;i++){
+        scanf("%f",&a[i]);
     }
-
+    resultados = calculcula_estatisticas(nro_elementos,a);
+    printf("Média: %f\n",resultados.media);
+    printf("Minimo: %f\n",resultados.minimo);
+    printf("Maximo: %f\n",resultados.maximo);
 
 
 
